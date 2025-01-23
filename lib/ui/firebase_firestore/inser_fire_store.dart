6f919fd20e91649716cd74bd0a2c9ac51c880e7a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-24 03:25:25 +0500

    inser firestore

---
+++ lib/ui/firebase_firestore/inser_fire_store.dart
@@ -0,0 +1,72 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+import 'package:flutter/material.dart';
+import 'package:real_time_db_firebase/ui/auth/login_screen.dart';
+import 'package:real_time_db_firebase/utils/utils.dart';
+
+
+class InsertFireStoreScreen extends StatefulWidget {
+  const InsertFireStoreScreen({super.key});
+
+  @override
+  State<InsertFireStoreScreen> createState() => _InsertFireStoreScreenState();
+}
+
+class _InsertFireStoreScreenState extends State<InsertFireStoreScreen> {
+
+  final auth = FirebaseAuth.instance ;
+  final ref = FirebaseDatabase.instance.ref('Post');
+
+  final  fireStore = FirebaseFirestore.instance.collection('users');
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Post'),
+        actions: [
+          IconButton(onPressed: (){
+            auth.signOut().then((value){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()));
+            }).onError((error, stackTrace){
+              Utils().toastMessage(error.toString());
+            });
+          }, icon: Icon(Icons.logout_outlined),),
+          SizedBox(width: 10,)
+        ],
+      ),
+      body: Column(
+        children: [
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+          String id = DateTime.now().millisecondsSinceEpoch.toString() ;
+          fireStore.doc(id).set({
+            'full_name': "asdf", // John Doe
+            'company': "adsf", // Stokes and Sons
+            'age': 12  ,
+            'id':id
+          });
+
+        } ,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
