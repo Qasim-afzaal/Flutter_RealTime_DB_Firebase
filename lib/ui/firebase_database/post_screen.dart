82b720d8332087252e6ef5d22f1d71ada9758469
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-25 03:19:10 +0500

    code refactor

--- lib/ui/firebase_database/post_screen.dart
+++ lib/ui/firebase_database/post_screen.dart
@@ -6,7 +6,6 @@ import 'package:real_time_db_firebase/ui/auth/login_screen.dart';
 import 'package:real_time_db_firebase/ui/firebase_database/add_posts.dart';
 import 'package:real_time_db_firebase/utils/utils.dart';
 
-
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
 
@@ -15,17 +14,8 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
-
-  final auth = FirebaseAuth.instance ;
+  final auth = FirebaseAuth.instance;
   final ref = FirebaseDatabase.instance.ref('Post');
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,96 +23,79 @@ class _PostScreenState extends State<PostScreen> {
       appBar: AppBar(
         title: Text('Post'),
         actions: [
-          IconButton(onPressed: (){
-            auth.signOut().then((value){
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()));
-            }).onError((error, stackTrace){
-              Utils().toastMessage(error.toString());
-            });
-          }, icon: Icon(Icons.logout_outlined),),
-          SizedBox(width: 10,)
+          IconButton(
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+              }).catchError((error) {
+                Utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
         ],
       ),
       body: Column(
         children: [
           Expanded(
             child: FirebaseAnimatedList(
-                query: ref,
-                defaultChild: Text('Loading'),
-                itemBuilder: (context, snapshot, animation, index){
-                  return   ListTile(
-                    title: Text(snapshot.child('title').value.toString()),
-                    subtitle: Text(snapshot.child('id').value.toString()),
-                    trailing:  PopupMenuButton(
-                        color: Colors.white,
-                        elevation: 4,
-                        padding: EdgeInsets.zero,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(2))),
-                        icon: Icon(Icons.more_vert,),
-                        itemBuilder: (context) => [
-                          PopupMenuItem(
-                            value: 1,
-                            child: PopupMenuItem(
-                              value: 2,
-                              child:  ListTile(
-                                onTap: (){
-                                  Navigator.pop(context);
-
-                                  ref.child(snapshot.child('id').value.toString()).update(
-                                      {
-                                        'title' : 'nice world'
-                                      }).then((value){
-
-                                  }).onError((error, stackTrace){
-                                    Utils().toastMessage(error.toString());
-                                  });
-
-                                },
-                                leading: Icon(Icons.edit),
-                                title: Text('Edit'),
-                              ),
-                            ),
-                          ),
-                          PopupMenuItem(
-                            value: 2,
-                            child:  ListTile(
-                              onTap: (){
-                                Navigator.pop(context);
-
-                                // ref.child(snapshot.child('id').value.toString()).update(
-                                //     {
-                                //       'ttitle' : 'hello world'
-                                //     }).then((value){
-                                //
-                                // }).onError((error, stackTrace){
-                                //   Utils().toastMessage(error.toString());
-                                // });
-                                ref.child(snapshot.child('id').value.toString()).remove().then((value){
-
-                                }).onError((error, stackTrace){
-                                  Utils().toastMessage(error.toString());
-                                });
-                              },
-                              leading: Icon(Icons.delete_outline),
-                              title: Text('Delete'),
-                            ),
-                          ),
-                        ]),
-                  );
-                }
+              query: ref,
+              defaultChild: Center(child: CircularProgressIndicator()),
+              itemBuilder: (context, snapshot, animation, index) {
+                return ListTile(
+                  title: Text(snapshot.child('title').value.toString()),
+                  subtitle: Text(snapshot.child('id').value.toString()),
+                  trailing: PopupMenuButton(
+                    color: Colors.white,
+                    elevation: 4,
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
+                    icon: Icon(Icons.more_vert),
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        value: 1,
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                            ref.child(snapshot.child('id').value.toString()).update({'title': 'nice world'}).then((_) {
+                              Utils().toastMessage('Post updated successfully.');
+                            }).catchError((error) {
+                              Utils().toastMessage(error.toString());
+                            });
+                          },
+                          leading: Icon(Icons.edit),
+                          title: Text('Edit'),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: 2,
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                            ref.child(snapshot.child('id').value.toString()).remove().then((_) {
+                              Utils().toastMessage('Post deleted successfully.');
+                            }).catchError((error) {
+                              Utils().toastMessage(error.toString());
+                            });
+                          },
+                          leading: Icon(Icons.delete_outline),
+                          title: Text('Delete'),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) => AddPostScreen()));
-        } ,
+        },
         child: Icon(Icons.add),
       ),
     );
   }
-
 }
