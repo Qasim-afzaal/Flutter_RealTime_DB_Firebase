e0396e8e0fa1d8dedf06953b8781923273c3bd4b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-10 04:21:38 +0500

    update color

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
-
-
-  void toastMessage(String message){
+  static void showToast(String message, {Color backgroundColor = const Color.fromARGB(255, 201, 24, 11), Color textColor = Colors.white, double fontSize = 16.0}) {
     Fluttertoast.showToast(
-        msg: message,
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: backgroundColor,
+      textColor: textColor,
+      fontSize: fontSize,
     );
   }
 }
