ddef43bf37690a7668dfebf3e623f0d07286de09
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-10 04:21:55 +0500

    use hexa code

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
-  static void showToast(String message, {Color backgroundColor = const Color.fromARGB(255, 201, 24, 11), Color textColor = Colors.white, double fontSize = 16.0}) {
+  static void showToast(String message, {Color backgroundColor = const Color(0xFFC9180B), Color textColor = const Color(0xFFFFFFFF), double fontSize = 16.0}) {
     Fluttertoast.showToast(
       msg: message,
       toastLength: Toast.LENGTH_SHORT,
